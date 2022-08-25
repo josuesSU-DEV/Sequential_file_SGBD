439169a9fa6ee7263c003b1ddd870f3916dac8a0
josueSU-UNSA <82920949+josueSU-UNSA@users.noreply.github.com>
2022-08-24 20:06:47 -0500

    Now the seq_file store pointer

--- Seq_file.h
+++ Seq_file.h
@@ -1,22 +1,30 @@
 #include<iostream>
-#include<fstream>
+// #include<fstream>
 #include"Over_flow_block.h"
 #include<list>
 
 class Seq_file{
     private:
-        list<Record_instructor>m_seq_file;
+        list<Record_instructor*>m_seq_file;
         Over_flow_block collector;
         unsigned int m_capicity_block;
     public:
+        // Seq_file(){
+            
+        //     // this->m_capicity_block=capicity_block;
+        // }
         Seq_file(unsigned int capicity_block){
+            
             this->m_capicity_block=capicity_block;
         }
-        void insert(int position,Record_instructor t){
+        // bool is_db_void(){
+
+        // }
+        void insert(int position,Record_instructor* t){
             auto it = this->m_seq_file.begin();
             // ++it;
             // advance(it,pos);
-            if(this->m_seq_file.size()*(*it).size_record()+(*it).size_record()<=this->m_capicity_block){
+            if(this->m_seq_file.size()*(*it)->size_record()+(*it)->size_record()<=this->m_capicity_block){
                 // this->m_seq_file.insert(it,*t);
                 if(position>=this->m_seq_file.size()){
                     this->m_seq_file.push_back(t);
@@ -35,7 +43,7 @@ class Seq_file{
                 
             }
         }
-        void insert(Record_instructor t){
+        void insert(Record_instructor *t){
             if(this->m_seq_file.empty()){
                 this->m_seq_file.push_front(t);
                 return;
@@ -44,10 +52,11 @@ class Seq_file{
 
             
             auto it_2=this->m_seq_file.begin();
-            if(this->m_seq_file.size()*(*it_2).size_record()+(*it_2).size_record()<=this->m_capicity_block){
+            if(this->m_seq_file.size()*(*it_2)->size_record()+(*it_2)->size_record()<=this->m_capicity_block){
                 int count_index=0;
                 for(;it_2!=this->m_seq_file.end();it_2++){
-                    if(stoi((*it_2).m_ID)>stoi(t.m_ID)){
+                    if(stoi((*it_2)->m_ID)==stoi(t->m_ID))return;
+                    if(stoi((*it_2)->m_ID)>stoi(t->m_ID)){
                         // lista_numerica.insert(it_2,int_input);
                         break;
                     }
@@ -68,26 +77,64 @@ class Seq_file{
         void print(){
             for( auto it = this->m_seq_file.begin(); it != this->m_seq_file.end(); ++it )
             {
-                (*it).print();
+                (*it)->print();
                 cout<<endl;
             }
         }
         void charge_to_disk(){
+            
+            
+            
+            
+            
+            if(this->m_seq_file.empty())return;
+
+
+            // ifstream check_the_block;
+            // check_the_block.open("instructor.db");
+            // char first_character;
+            // check_the_block>>first_character;
+            
+            // string first_line;
+            // if(first_character!='\0'){
+                
+            //     // El size del block:
+            //     getline(file,first_line);
+            //     first_line=first_character+first_line;
+
+
+
+
+            // }
+            // check_the_block.close();
+            
+            
+            
             ofstream file;
             // file.open("E:/BBDD2_Lab/DDBB_blocks/instructor.db");
 
             file.open("instructor.db");
+            
+            
+            
+            
+            
+            
+            file<<this->m_capicity_block<<endl;
+            
             auto it = this->m_seq_file.begin();
-            file<<(*it).return_names_columns_record_str_format()<<"\n";
+            file<<(*it)->return_names_columns_record_str_format()<<endl;
+            
             for( ; it != this->m_seq_file.end(); ++it )
             {
-                auto it_aux=it;
-                it_aux++;
-                if (it_aux!=this->m_seq_file.end())file<<(*it).return_record_str_format()<<"\n";
-                else file<<(*it).return_record_str_format();
+                file<<(*it)->return_record_str_format();
+                if((*it)!=this->m_seq_file.back())file<<endl;
+                
             }
-            this->collector.charge();
             file.close();
+            if(!this->collector.m_overlow_block.empty())this->collector.charge();
+            
+            
         }
         
 };
