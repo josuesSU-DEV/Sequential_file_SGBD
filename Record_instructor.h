67ca40ad62585ba060f4fdf39438e42819002b07
josueSU-UNSA <82920949+josueSU-UNSA@users.noreply.github.com>
2022-08-24 20:07:20 -0500

    Correction mistake of the write

--- Record_instructor.h
+++ Record_instructor.h
@@ -10,6 +10,41 @@ class Record_instructor{
         string m_dept_name;
         double m_salary;
     public:
+        Record_instructor(string tuple_record){
+            this->m_ID="";
+            int i=0;
+            for (;i<tuple_record.size();i++){
+                if(tuple_record[i]==','){
+                    i++;
+                    break;
+                }
+                this->m_ID+=tuple_record[i];
+            }
+            this->m_name="";
+            for (;i<tuple_record.size();i++){
+                if(tuple_record[i]==','){
+                    i++;
+                    break;
+                }
+                this->m_name+=tuple_record[i];
+            }
+            this->m_dept_name="";
+            for (;i<tuple_record.size();i++){
+                if(tuple_record[i]==','){
+                    i++;
+                    break;
+                }
+                this->m_dept_name+=tuple_record[i];
+            }
+            this->m_salary=0;
+            string salary_str="";
+            for (;i<tuple_record.size();i++){
+                
+                salary_str+=tuple_record[i];
+            }
+            this->m_salary=stod(salary_str);
+        }
+
         Record_instructor(string ID,string name,string dept_name,double salary){
             this->m_ID=ID;
             this->m_name=name;
