d7f4cdb4a31ae40a6ab00fc52d8f960a723bed36
josueSU-UNSA <82920949+josueSU-UNSA@users.noreply.github.com>
2022-08-24 20:07:59 -0500

    Correction mistake of write in file

--- Over_flow_block.h
+++ Over_flow_block.h
@@ -3,10 +3,10 @@
 #include"Record_instructor.h"
 class Over_flow_block{
     public:
-        stack<Record_instructor>m_overlow_block;
+        stack<Record_instructor*>m_overlow_block;
     public:
         Over_flow_block(){}
-        void insert(Record_instructor t){
+        void insert(Record_instructor *t){
             this->m_overlow_block.push(t);
         }
         void charge(){
@@ -14,11 +14,13 @@ class Over_flow_block{
             // file.open("E:/BBDD2_Lab/DDBB_blocks/overflow_block_instructor.db");
 
             file.open("overflow_block_instructor.db");
-            file<<m_overlow_block.top().return_names_columns_record_str_format()<<"\n";
+            file<<m_overlow_block.top()->return_names_columns_record_str_format()<<endl;
             while(!this->m_overlow_block.empty()){
-                if(this->m_overlow_block.size()==1)file<<this->m_overlow_block.top().return_record_str_format();
-                else file<<this->m_overlow_block.top().return_record_str_format()<<"\n";
-                
+                file<<this->m_overlow_block.top()->return_record_str_format();
+                if(this->m_overlow_block.size()!=1)file<<endl;
+                // if(this->m_overlow_block.size()==1)file<<this->m_overlow_block.top()->return_record_str_format();
+                // else file<<this->m_overlow_block.top()->return_record_str_format()<<endl;
+                // file<<this->m_overlow_block.top()->return_record_str_format()<<endl;
                 this->m_overlow_block.pop();
             }
             file.close();
